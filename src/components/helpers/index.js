c335e066a0c034b911084329b8aa2e04eba55c55
Sebastian <sanchezz3s47@gmail.com>
2024-05-15 14:43:33 -0500

    change format date

--- src/components/helpers/index.js
+++ src/components/helpers/index.js
@@ -1,9 +1,13 @@
-
 export const formatDate = (date) => {
-
-    const formatDate = new Date(date ? date.seconds * 1000 : '')
-    const time = `${formatDate.getHours()}:${formatDate.getMinutes()}`
-    const options = {  month: 'long', day: 'numeric' }
-    const newDate = formatDate.toLocaleDateString('es-ES', options)
-    return date ?  `${newDate} - ${time}` : ''
+    const formatDate = new Date(date ? date.seconds * 1000 : '');
+    let hours = formatDate.getHours() % 12;
+    hours = hours === 0 ? 12 : hours; 
+    const minutes = formatDate.getMinutes() < 10 ? '0' + formatDate.getMinutes() : formatDate.getMinutes();
+    const ampm = formatDate.getHours() < 12 ? 'AM' : 'PM';
+    const time = `${hours}:${minutes} ${ampm}`;
+    const options = { month: 'long', day: 'numeric' };
+    const newDate = formatDate.toLocaleDateString('es-ES', options);
+    
+    return date ?  `${newDate} - ${time}` : '...';
 }
+
